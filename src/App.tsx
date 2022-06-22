68e4770f3759b3edc599b5ece417a4c09127b6ef
YuukiHnf <68813176+YuukiHnf@users.noreply.github.com>
2022-06-22 19:50:37 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./styles.css";
+import InitalCalendar from "./Calendar/InitalCalendar";
+
+export default function App() {
+  return (
+    <div className="App">
+      <InitalCalendar />
+    </div>
+  );
+}
